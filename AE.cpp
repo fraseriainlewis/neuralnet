b5f62d9c3302afc55dbd7e3e9937e7749b1a58a8
Fraser Lewis <fraser.iain.lewis@gmail.com>
2019-08-28 18:14:37 +0200

    added autoencoder examples

---
+++ AE.cpp
@@ -0,0 +1,131 @@
+#include <mlpack/methods/ann/layer/layer.hpp>
+#include <mlpack/methods/ann/loss_functions/mean_squared_error.hpp>
+#include <mlpack/methods/ann/init_rules/const_init.hpp>
+#include <mlpack/methods/ann/ffn.hpp>
+#include <ensmallen.hpp>
+#include <mlpack/prereqs.hpp>
+
+#define DEBU
+
+using namespace mlpack;
+using namespace mlpack::ann;
+using namespace arma;
+using namespace std;
+
+int main()
+{
+
+// set the random number seed - e.g. shuffling or random starting points in optim
+arma::arma_rng::set_seed(1000);//1000
+//arma::arma_rng::set_seed_random();
+
+/**************************************************************************************************/
+/** Load the training set - separate files for features and lables (regression) **/
+/** note - data is read into matrix in column major, e.g. each new data point is a column - opposite from data file **/
+arma::mat trainData, trainLabels;
+uword i,j;
+data::Load("AEdata.csv", trainData, true);//  
+data::Load("AEdata.csv", trainLabels, true);// 
+
+// print out to check the data is read in correctly
+arma::cout << "n rows="<<trainData.n_rows <<" n cols="<< trainData.n_cols << arma::endl;
+//arma::cout << "n rows="<< trainLabels.n_rows <<" n cols"<< trainLabels.n_cols << arma::endl;
+
+/**************************************************************************************************/
+/** MODELS - linear autoencoder with one hidden layer with one node **/
+
+const size_t inputSize=trainData.n_rows;// =8 
+const size_t outputSize=trainData.n_rows;// =8
+const size_t hiddenLayerSize=1;//
+
+/** could build model direct in layers - this works fine but more elegant using sequential here see below **/
+/* not using this code *//*
+FFN<MeanSquaredError<>,RandomInitialization> model0(MeanSquaredError<>(),RandomInitialization(-1,1));
+model0.Add<Linear<> >(inputSize, hiddenLayerSize);
+model0.Add<IdentityLayer<> >();
+model0.Add<Linear<> >(hiddenLayerSize, outputSize);
+model0.Add<IdentityLayer<> >();
+*/
+
+FFN<MeanSquaredError<>,RandomInitialization> model1(MeanSquaredError<>(),RandomInitialization(-1,1));
+// Encoder
+Sequential<>* encoder = new Sequential<>();
+encoder->Add<Linear<> >(inputSize, hiddenLayerSize);
+encoder->Add<IdentityLayer<> >();
+// Decoder
+Sequential<>* decoder = new Sequential<>();
+decoder->Add<Linear<> >(hiddenLayerSize,outputSize);
+decoder->Add<IdentityLayer<> >();
+
+// now link all the layers together
+model1.Add<IdentityLayer<> >(); // layer-0 this is needed a can't start with sequential as first layer
+model1.Add(encoder);            // layer-1
+model1.Add(decoder);            // layer-2
+
+// set up optimizer 
+ens::Adam opt(0.001,trainData.n_cols, 0.9, 0.999, 1e-8, 0, 1e-5,false,true); //https://ensmallen.org/docs.html#adam
+                 
+
+arma::cout<<"-------empty params------------------------"<<arma::endl;//empty params as not yet allocated
+arma::cout << model1.Parameters() << arma::endl;
+model1.Train(trainData, trainLabels,opt);
+arma::cout<<"-------final params------------------------"<<arma::endl;
+arma::cout << model1.Parameters() << arma::endl;
+
+// Use the Predict method to get the assignments.
+double loss1=model1.Evaluate(trainData,trainLabels);
+arma::mat assignments;
+model1.Predict(trainData, assignments);
+
+//check forward - full forward, all layers, should be same as Predict
+arma::mat assignments2;
+model1.Forward(trainData, assignments2,0,2);// start layer 0, stop layer 2
+
+// manual computation of MSE loss - from predict
+double loss2=0;
+for(i=0;i<assignments.n_cols;i++){
+  for(j=0;j<assignments.n_rows;j++){
+    loss2+= (assignments(j,i)-trainLabels(j,i))*(assignments(j,i)-trainLabels(j,i));
+  }
+}
+
+// manual computation of MSE loss - from predict
+double loss3=0;
+for(i=0;i<assignments2.n_cols;i++){
+  for(j=0;j<assignments2.n_rows;j++){
+    loss3+= (assignments2(j,i)-trainLabels(j,i))*(assignments2(j,i)-trainLabels(j,i));
+  }
+}
+
+
+// check loss manually
+loss1=loss1;//get mean error from predict
+loss2=loss2/ (double)assignments.n_cols;//get mean error from forward
+loss3=loss3/ (double)assignments2.n_cols;//get mean error from forward
+arma::cout<<"MSE auto="<<loss1<<arma::endl;
+arma::cout<<"MSE manual predict="<<loss2<<arma::endl;
+arma::cout<<"MSE manual forward(0,2)="<<loss3<<arma::endl;
+
+
+
+// now compute just encoded data - push into layer 0 and out of layer 1
+arma::mat assignments3;
+model1.Forward(trainData, assignments3,0,1);// this works 
+arma::cout<<"FORWARD SEQ 0 1 ncol"<<assignments3.n_cols<<" FORWARD SEQ 0 1 nrow"<<assignments3.n_rows<<arma::endl;
+
+// now compute decoded data - push encoded data into layer 2 and out of layer 2
+arma::mat assignments4;
+model1.Forward(assignments3, assignments4,2,2);// this works but 
+arma::cout<<"FORWARD 0 1 ncol"<<assignments4.n_cols<<" FORWARD 1 nrow"<<assignments4.n_rows<<arma::endl;
+
+//now send to disk - encoded data
+mat ass4T = assignments3.t();
+ass4T.save("c_encoded1.csv", csv_ascii); 
+
+//now send to disk - decoded data
+mat ass5T = assignments4.t();
+ass5T.save("c_decoded8.csv", csv_ascii); 
+
+
+
+}
---
+++ AE_torch.py
@@ -0,0 +1,159 @@
+# -*- coding: utf-8 -*-
+import torch
+import torch.utils.data as utils
+from torch.autograd import Variable
+# general helper libraries
+import pathlib
+import os
+import pandas as pd
+import numpy as np
+from numpy.random import seed # numpy random number set function
+
+np.random.seed(9990)# 9990
+torch.manual_seed(9990) # 9990
+
+# read the data - note read in using pandas then convert from dataframe to numpy array then torch tensor
+features=pd.read_csv("AEdata.csv",delimiter=",",header=None) 
+featuresnp=(features.to_numpy())
+x = torch.from_numpy(featuresnp).double() # the cast to double is needed
+
+labels=pd.read_csv("AEdata.csv",delimiter=",",header=None) # 1000 observations
+labelsnp=(labels.to_numpy())
+y = torch.from_numpy(labelsnp).double()
+
+#print(y)
+print(y.shape)
+#print(x)
+print(x.shape)
+
+my_dataset = utils.TensorDataset(x,y) # create your datset
+
+dataset = utils.DataLoader(my_dataset,batch_size=438) # create your dataloader
+
+
+model = torch.nn.Sequential(
+	torch.nn.Linear(8, 1),
+	torch.nn.Identity(), # also Identity
+	torch.nn.Linear(1, 8),
+	torch.nn.Identity() # also Identity
+      )
+
+print(model)
+print(model[0:2:1])
+
+loss_fn = torch.nn.MSELoss(reduction='mean')
+
+model=model.double()
+
+# Use the optim package to define an Optimizer that will update the weights of
+# the model for us. Here we will use Adam; the optim package contains many other
+# optimization algoriths. The first argument to the Adam constructor tells the
+# optimizer which Tensors it should update. 
+learning_rate = 1e-3
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+#optimizer=torch.optim.RMSprop(model.parameters(), lr=0.01, alpha=0.99, eps=1e-08, weight_decay=0, momentum=0, centered=False)
+curloss=1e+300
+abserror=1e-05
+maxiters=100000
+
+ #img = img.view(img.size(0), -1)
+ #       img = Variable(img).cuda()
+ #       # ===================forward=====================
+ #       output = model(img)
+ #       loss = criterion(output, img)
+
+#data   = Variable(data,requires_grad=False)
+ #   target = Variable(target.long(),requires_grad=False)
+minLossOverall=1e+300
+
+for t in range(maxiters): # for each epoch - all training data run through once
+    running_loss=0.0
+    i=0
+    for input, target in dataset: # for each batch of training data update the current weights
+        optimizer.zero_grad()
+        output = model(input)
+        loss = loss_fn(output, target)
+        loss.backward()
+        optimizer.step()
+        # print statistics
+        running_loss += loss.item()
+        #if i % 25 == (25-1):    # print every 25 mini-batches
+        #    print('[%d, %5d] loss: %.3f' %
+        #          (t + 1, i + 1, running_loss))
+        #    #running_loss = 0.0
+        i=i+1  
+    #print("t=",t," ",i," ",running_loss)
+    if np.absolute(running_loss-curloss) <abserror:
+        # have good enough solution so stop
+        print("BREAK: iter=",t," ","loss=",running_loss,"\n")
+        break
+    else: 
+        curloss=running_loss # copy loss
+
+    if ((t%100)==0):
+        print(t, curloss)
+
+
+# print out parameters
+print("---PARAMETERS-----\n")
+for name, param in model.named_parameters():
+    if param.requires_grad:
+        print (name, param.data)
+
+
+#modelNew=model[]
+preds = model(x)
+#print(preds.shape)
+prednp=preds.detach().numpy()
+#print("first 10 and last 10 probabilities output from model\n")
+#print(prednp[0:10:1,:])
+
+nrows=prednp.shape[0]
+ncols=prednp.shape[1]
+myloss=0.0
+for i in range(nrows):
+	for j in range(ncols):
+		myloss+= (prednp[i,j]-labelsnp[i,j])*(prednp[i,j]-labelsnp[i,j])
+
+print("MANUAL LOSS=",myloss/y.shape[0],"\n")
+
+## print encoding of 8-dim vector into
+new_model = torch.nn.Sequential(*list(model.children())[0:2:1]) ## only keep first two layers
+#print(new_model(x))
+encodepreds2dim=new_model(x)
+
+
+# print out parameters
+print("---PARAMETERS-----\n")
+for name, param in new_model.named_parameters():
+    if param.requires_grad:
+        print (name, param.data)
+
+new_model2 = torch.nn.Sequential(*list(model.children())[2:4:1]) ## only keep first two layers
+#print(new_model2(encodepreds2dim))
+decodepreds8dim=new_model2(encodepreds2dim);
+
+# print out parameters
+print("---PARAMETERS-----\n")
+for name, param in new_model2.named_parameters():
+    if param.requires_grad:
+        print (name, param.data)
+
+print("-----------\n")
+# get encoded values in np
+encodepreds2dimnp=encodepreds2dim.detach().numpy()
+print(encodepreds2dimnp[0:10:1,:])
+
+encodepd=pd.DataFrame(data=encodepreds2dimnp)
+
+# get decoded values in np
+decodepreds8dimnp=decodepreds8dim.detach().numpy()
+print(decodepreds8dimnp[0:10:1,:])
+
+# this works arithmetically just fine!
+encodepd=pd.DataFrame(data=encodepreds2dimnp)
+decodepd=pd.DataFrame(data=decodepreds8dimnp)
+encodepd.to_csv("torch_encoded.csv", encoding='utf-8', index=False)
+decodepd.to_csv("torch_decoded.csv", encoding='utf-8', index=False)
+
+
